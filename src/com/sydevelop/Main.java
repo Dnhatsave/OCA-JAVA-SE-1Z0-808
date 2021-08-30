d2ec8bf1e40d84264563110627912588918710e3
dercioink <dercionhatsave@gmail.com>
2021-08-30 20:06:52 +0200

    Megabytes to Kilobytes exercice

--- src/com/sydevelop/Main.java
+++ src/com/sydevelop/Main.java
@@ -3,11 +3,15 @@ package com.sydevelop;
 public class Main {
 
     public static void main(String[] args) {
-        SpeedConverter.printConversion(1.5);
-        SpeedConverter.printConversion(10.25);
-        SpeedConverter.printConversion(-5.6);
-        SpeedConverter.printConversion(25.42);
-        SpeedConverter.printConversion(75.114);
+//        SpeedConverter.printConversion(1.5);
+//        SpeedConverter.printConversion(10.25);
+//        SpeedConverter.printConversion(-5.6);
+//        SpeedConverter.printConversion(25.42);
+//        SpeedConverter.printConversion(75.114);
+
+        MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
+        MegaBytesConverter.printMegaBytesAndKiloBytes(-1024);
+        MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
     }
 
 }
